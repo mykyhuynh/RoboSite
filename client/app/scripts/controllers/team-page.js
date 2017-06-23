48dd9b6883ce0287ff4c1b3204b7b8aa59d0fd66
Cameron Forsythe <cameronForsythe@camerons-mbp.gateway.pace.com>
2017-06-23 18:21:37 -0400

    tabs working

--- client/app/scripts/controllers/team-page.js
+++ client/app/scripts/controllers/team-page.js
@@ -1,17 +1,46 @@
-'use strict';
+// 'use strict';
 
-/**
- * @ngdoc function
- * @name yoApp.controller:TeamPageCtrl
- * @description
- * # TeamPageCtrl
- * Controller of the yoApp
- */
-angular.module('yoApp')
-  .controller('TeamPageCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+// /**
+//  * @ngdoc function
+//  * @name yoApp.controller:TeamPageCtrl
+//  * @description
+//  * # TeamPageCtrl
+//  * Controller of the yoApp
+//  */
+// angular.module('yoApp')
+//   .controller('TeamPageCtrl', function () {
+//     this.awesomeThings = [
+//       'HTML5 Boilerplate',
+//       'AngularJS',
+//       'Karma'
+//     ];
+//   });
+
+(function() {
+  'use strict';
+  angular
+    .module('yoApp')
+    .controller('TeamPageCtrl', TeamPageCtrl);
+
+    //all the stuff you want
+  TeamPageCtrl.$inject = ['$scope', 'Movie'];
+
+  function TeamPageCtrl($scope, Movie){
+  //example
+    var vm = $scope;
+
+    vm.movie = {};
+
+    vm.saveMovie = saveMovie;
+
+    function saveMovie() {
+      Movie.post(vm.movie).then(function() {
+        $location.path('/movies');
+      });
+    }
+
+    //mentor stuff
+	vm.movieObject = Movie.getList().$object;
+
+  }
+})();
