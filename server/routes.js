e309c99d1b7ab72751ea917b37751a61e0cc6a0d
flamingunicorn <cameron.forsythe1@gmail.com>
2017-06-27 18:32:01 -0400

    scroll icon

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,5 @@
 //make routes
 module.exports = {
 	'/swang' : require('./controllers/movieController.js'),
-	'/people' : require('./controllers/peopleController.js'),
-	'/photos' : require('./controllers/photoController.js')
+	'/people' : require('./controllers/peopleController.js')
 }
